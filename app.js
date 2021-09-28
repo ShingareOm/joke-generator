8fa3bfc7915ad13e6d33385c09a7b171eb3a749d
Atharva Shinde <atharvashinde179@gmail.com>
2021-09-29 01:02:04 +0530

    wip

---
+++ app.js
@@ -0,0 +1,22 @@
+const jokeText = document.getElementById('joke');
+const jokebtn = document.getElementById('random');
+
+jokebtn.addEventListener('click', generatejokes)
+
+// generatejokes()
+
+async function generatejokes(){
+    const res = await fetch(
+        "https://v2.jokeapi.dev/joke/Any"
+    )
+    const data = await res.json();
+    console.log(data)
+    // let joke = ""
+    // if(data.joke == undefined){
+    //     joke = `${data.setup} <br/> ${data.delivery}`
+    // }
+    // else{
+    //     joke = data.joke
+    // }
+    jokeText.innerHTML = joke
+}
